4e1d292ac6620e25a30470a5452a9659916f9789
oznurolcek <olcekoznur@gmail.com>
2023-08-04 00:32:15 +0300

    checkpoint

--- ToDoApp/ui/views/MainPage.swift
+++ ToDoApp/ui/views/MainPage.swift
@@ -25,7 +25,11 @@ class MainPage: UIViewController {
         todos = [
             ToDos(id: 1, title: "Ödev", description: "Ödevi bitir", deadline: "2 Ağustos, Çarşamba"),
             ToDos(id: 2, title: "Doktor", description: "Doktor randevu", deadline: "10 Ağustos, Perşembe"),
-            ToDos(id: 3, title: "Toplantı", description: "İş toplantısı", deadline: "25 Ağustos, Cuma")
+            ToDos(id: 3, title: "Toplantı", description: "İş toplantısı", deadline: "25 Ağustos, Cuma"),
+            ToDos(id: 4, title: "Sipariş", description: "Sipariş ver", deadline: "12 Ağustos, Cumartesi"),
+            ToDos(id: 5, title: "Kitap", description: "Kitap oku", deadline: "15 Ağustos, Salı"),
+            ToDos(id: 6, title: "Buluşma", description: "Arkadaşlarla buluş", deadline: "9 Ağustos, Çarşamba")
+            
             ]
               
     }
